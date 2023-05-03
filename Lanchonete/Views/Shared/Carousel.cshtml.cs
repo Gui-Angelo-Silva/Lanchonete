79c21c685fa1dbde855cc76ae29d1e68ea6e24a6
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-05-03 16:13:12 -0300

    Segundo Commit

---
+++ Lanchonete/Views/Shared/Carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lanchonete.Views.Shared
+{
+    public class CarouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
